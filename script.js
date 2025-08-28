7b0cc48e4dbf9a307ec9479db53ed011b070ab40
kaiakane <kkanethree@gmail.com>
2025-08-28 16:03:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ async function populateDays() {
   });
 }
 
-// Populate activities for a specific day
+// Populate activities for a specific day{
 console.log("Activity raw:", act);
 async function populateActivities() {
   const res = await fetch(csvUrl);
